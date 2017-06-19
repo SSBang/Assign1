53726b97146e378c39025b2a3a407e39af636795
unknown <SSBang>
2017-06-19 14:27:27 +0900

    svm_2_try

--- cs231n/classifiers/linear_svm.py
+++ cs231n/classifiers/linear_svm.py
@@ -33,14 +33,13 @@ def svm_loss_naive(W, X, y, reg):
         scores = X[i].dot(W)
         correct_class_score = scores[y[i]]
         for j in range(num_classes):
-            margin_sum = 0
             if j == y[i]:
-                ds[i,j] = -1
+                ds[i,j] = 1-num_classes
                 continue
             margin = scores[j] - correct_class_score + 1  # note delta = 1
             if margin > 0:
                 loss += margin
-                ds[i,j] = -1
+                ds[i,j] = 1
             
 
     # Right now the loss is a sum over all training examples, but we want it
